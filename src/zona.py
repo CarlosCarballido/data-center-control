db2354a45b31fd7bd2bc69175e230581625e69d5
CarlosCarballido <carlos.carballido.placer@uvigomotorsport.com>
2024-10-24 16:13:59 +0200

    Add zona class with comparison methods

---
+++ src/zona.py
@@ -0,0 +1,22 @@
+class zona:
+    def __init__(self, nombre, nivel) -> None:
+        self.nombre = nombre
+        self.nivel = nivel
+        
+    def __str__(self) -> str:
+        return f'zona: {self.nombre} nivel: {self.nivel}'
+    
+    def __repr__(self) -> str:
+        return f'zona: {self.nombre} nivel: {self.nivel}'
+    
+    def __lt__(self, o: object) -> bool:
+        return self.nivel < o.nivel
+    
+    def __le__(self, o: object) -> bool:
+        return self.nivel <= o.nivel
+    
+    def __gt__(self, o: object) -> bool:
+        return self.nivel > o.nivel
+    
+    def __ge__(self, o: object) -> bool:
+        return self.nivel >= o.nivel
